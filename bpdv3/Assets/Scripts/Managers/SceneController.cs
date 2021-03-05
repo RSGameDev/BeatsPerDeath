99b54e6783e58381ead55bd4aa6c5fdea79612f6
RSGameDev <rsgaming@hotmail.co.uk>
2021-03-05 01:29:08 +0000

    Code fix was less impactful as i originally thought it would be
    -The code fix was less urgent than i thought it was needed. The enemy movement was not affected, the commented out code just removed a dev helping functionality for the project.
    In the game (scene1) on the tile controller there is a tick box 'turn off dev tile value'. This will do the same as what the commented out code before was achieving. So this only needs to be ticked and it will do the same thing, on the tilecontroller game object in scene1.
    
    - A new player model has been added where the textures can be attached to the ears, head and limbs. So the player looks better now.
    
    - Devhelper tick box works a little better so that the beat and spawn counters are removed to now if we wish to see the game more from a players perspective. Which can be activated again from the tile controller game object's tick the box 'turn off dev tile value'. This can be renamed better in the future. 'turn off dev mode' something like this.
    
    - Tidy up of redundant items in the project. Things that i am confident we no longer have use for. I keep somethings in case we need to refer to them so i only removed things im sure are no longer needed.
    
    - Renamed Enemytype to shroom now. Better fitting. As there was an old shroom object this was why i didnt call it shroom also. As the old shroom was removed I felt it was now fine to call enemytype, shroom now.

--- bpdv3/Assets/Scripts/Managers/SceneController.cs
+++ bpdv3/Assets/Scripts/Managers/SceneController.cs
@@ -12,7 +12,7 @@ namespace Managers
     public class SceneController : Singleton<SceneController>
     {
         #region Private and Constant variables
-        
+
         [SerializeField] private GameObject _splashScreen;
         [SerializeField] private GameObject _warningScreen;
         [SerializeField] private GameObject _mainMenu;
@@ -20,29 +20,29 @@ namespace Managers
         [SerializeField] private GameObject _gameplayMenu;
         [SerializeField] private GameObject _soundsMenu;
         [SerializeField] private GameObject _creditsScreen;
-        [SerializeField] private int s_SplashDelay = 4;   //make const 4sec
+        [SerializeField] private int s_SplashDelay = 4; //make const 4sec
         [SerializeField] private int s_warningDelay = 3; //make const 3sec
         private const string s_startGameSceneName = "scene1";
-        private bool _isReferenced;           
-        
-        // TODO will be deleted, here for debug purposes
-        [SerializeField] private TextMeshProUGUI beatUiValue;
-        // TODO will be deleted, here for debug purposes
-        [SerializeField] private TextMeshProUGUI spawnUiValue;
+        private bool _isReferenced;
+
+        private TextMeshProUGUI beatHeader;
+        private TextMeshProUGUI beatUiValue;
+        private TextMeshProUGUI spawnHeader;
+        private TextMeshProUGUI spawnUiValue;
 
         private TileController tileController;
-        
+
         #endregion
 
         #region Public and Protected variables
-    
+
         public AudioManager audioManagerObj;
         public int CurrentSceneIndex { get; private set; }
-    
+
         #endregion
 
         #region Contructors
-        
+
         private new void Awake()
         {
             base.Awake();
@@ -60,24 +60,18 @@ namespace Managers
         {
             SetSceneSettings();
         }
-    
+
         #endregion
-         
+
         #region Private Methods
-    
+
         private void Update()
         {
             if (SceneManager.GetActiveScene().buildIndex == 1 && !_isReferenced)
             {
-                tileController = GameObject.FindWithTag("TileController").GetComponent<TileController>();
-                
                 _isReferenced = true;
-                var temp = GameObject.Find("BeatUITestValue (TMP)");
-                
-                beatUiValue = temp.GetComponent<TextMeshProUGUI>();
-
-                var temp1 = GameObject.Find("ScoreUITest Value (TMP) (1)");
-                spawnUiValue = temp1.GetComponent<TextMeshProUGUI>();
+                InitLevelOneReferences();
+                DevModeOnOrOff();
             }
 
             audioManagerObj.MusicLayering();
@@ -102,7 +96,35 @@ namespace Managers
             // TODO will be deleted, here for debug purposes
             BeatManager.Instance.AddListenerToAll(UpdateSpawnCountUI);
         }
-        
+
+        private void InitLevelOneReferences()
+        {
+            tileController = GameObject.FindWithTag("TileController").GetComponent<TileController>();
+            
+            var temp = GameObject.Find("Beat Header (TMP)");
+            beatHeader = temp.GetComponent<TextMeshProUGUI>();
+
+            var temp1 = GameObject.Find("BeatUITestValue (TMP)");
+            beatUiValue = temp1.GetComponent<TextMeshProUGUI>();
+
+            var temp2 = GameObject.Find("Score Header (TMP) (1)");
+            spawnHeader = temp2.GetComponent<TextMeshProUGUI>();
+
+            var temp3 = GameObject.Find("ScoreUITest Value (TMP) (1)");
+            spawnUiValue = temp3.GetComponent<TextMeshProUGUI>();
+        }
+
+        private void DevModeOnOrOff()
+        {
+            if (tileController.GetComponent<TileController>().turnOffDevTileValues)
+            {
+                beatHeader.gameObject.SetActive(false);
+                beatUiValue.gameObject.SetActive(false);
+                spawnHeader.gameObject.SetActive(false);
+                spawnUiValue.gameObject.SetActive(false);
+            }
+        }
+
         private IEnumerator DelayOnSplashScreen()
         {
             yield return new WaitForSeconds(s_SplashDelay);
@@ -123,16 +145,11 @@ namespace Managers
             LoadMainMenu();
         }
 
-        private void UpdateBeatUI() 
+        private void UpdateBeatUI()
         {
-            if (tileController.turnOffDevTileValues)
-            {
-                return;
-            }
-            
             var currentBeat = BeatManager.Instance.BeatIndex % 4;
             beatUiValue.text = currentBeat.ToString();
-            
+
             if (currentBeat == 0)
             {
                 currentBeat = 4;
@@ -142,30 +159,25 @@ namespace Managers
 
         private void UpdateSpawnCountUI()
         {
-            if (tileController.turnOffDevTileValues)
-            {
-                return;
-            }
-            
             var currentBeat = BeatManager.Instance.BeatIndex % 8;
             spawnUiValue.text = currentBeat.ToString();
-            
+
             if (currentBeat == 0)
             {
                 currentBeat = 8;
                 spawnUiValue.text = currentBeat.ToString();
             }
         }
-        
+
         #endregion
-        
+
         #region Public Methods
-        
+
         public void DelaySplashScreen()
         {
             StartCoroutine(DelayOnSplashScreen());
         }
-        
+
         public void LoadMainMenu()
         {
             _mainMenu.transform.localScale = Vector3.one;
@@ -175,7 +187,7 @@ namespace Managers
         public void StartGame()
         {
             SceneManager.LoadScene(s_startGameSceneName);
-            CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex+1;
+            CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
             audioManagerObj.SetGamePlaySceneSettings();
         }
 
@@ -192,7 +204,7 @@ namespace Managers
             _soundsMenu.transform.localScale = Vector3.zero;
             _creditsScreen.transform.localScale = Vector3.zero;
         }
-        
+
         public void LoadSoundsScreen()
         {
             _gameplayMenu.transform.localScale = Vector3.zero;
@@ -221,6 +233,7 @@ namespace Managers
         {
             SceneManager.LoadScene(CurrentSceneIndex + 1);
         }
+
         #endregion
     }
 }
